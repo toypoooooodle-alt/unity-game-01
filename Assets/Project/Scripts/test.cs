7c791ccb446229265ae0a602983bf82d8bfd9e5a
パコソン\kazum <toypoooooodle@gmail.com>
2025-12-02 23:15:02 +0900

    test

---
+++ Assets/Project/Scripts/test.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // This is a test script for version control
+    private void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+}
